519b0af6781ef1798c06a9c28737e00bbb911b5d
RiozeC <cietstrioze@gmail.com>
2022-04-27 05:42:17 +0630

    Updated Kana generation.; README.md fixes.

--- README.md
+++ README.md
@@ -5,25 +5,35 @@ This is my first Python project =w=
 ## Demo
 
 [KanaQuiz Telegram Bot](https://t.me/fio_kanaquiz_bot)<br>
-<image src='./screenshot.jpg' width='45%'>
+<image src='./screenshot.jpg' width='25%'>
+
+## Requirements
+
+- Python 3
+- Pip
 
 ## Run
 
 0. Grab this project.
 
-```
+```sh
 $ git clone https://github.com/riozec/kanaquiz-telegram-bot
 ```
 
 1. Create `config.yaml` file and throw your bot token there.
 
-```
-BOT_TOKEN="YOUR BOT TOKEN"
+```yaml
+BOT_TOKEN: "YOUR BOT TOKEN"
 ```
 
 2. Install all the requirements listed inside the `requirements.txt` file.
+
+```sh
+$ pip install -r requirements.txt
+```
+
 3. Bring up your terminal, run this command, and smash that enter key.
 
-```
-$ python src/main.py
+```sh
+$ python3 src/main.py
 ```
--- src/kana.py
+++ src/kana.py
@@ -75,21 +75,50 @@ kana = [
 def random_kana():
     idx = random.randint(0, len(kana) - 1)
     res = kana[idx].copy()
+    # reduce the probability of archaic form of we and wi occuring
+    if idx == 39 or idx == 40:
+        idx = random.randint(0, len(kana) - 1)
+        res = kana[idx].copy()
     # characters with small ya/yu/yo attachable
     if (
         4 < idx < 9 or idx == 26 or idx == 31 or 42 < idx < 48
     ) and random.random() < 0.15:
-        y = random.choice([["ゃ", "ャ", "ya"], ["ゅ", "ュ", "yu"], ["ょ", "ョ", "yo"]])
-        res[0] = res[0] + y[0]
-        res[1] = res[1] + y[1]
-        res[2] = res[2][:-1]  # romaji, strip the last character
-        if len(res[2]) == 2:  # if shi, chi or dji
-            res[2] += y[2][-1]  # attach only the vowel (a/u/o) => sha/shu/sho
+        # reduce the probability of dji gets modified
+        if idx == 45 and random.random() < 0.6:
+            pass
+        # (very rare) shi, chi => she, che
+        elif (idx == 6 or idx == 7) and random.random() < 0.15:
+            res[0] += "ぇ"
+            res[1] += "ェ"
+            res[2] = "she" if idx == 6 else "che"
         else:
-            if res[2] == "j":  # if ji
-                res[2] += y[2][-1]  # then attach only the vowel (a/u/o) => ja/ju/jo
+            y = random.choice([["ゃ", "ャ", "ya"], ["ゅ", "ュ", "yu"], ["ょ", "ョ", "yo"]])
+            res[0] = res[0] + y[0]
+            res[1] = res[1] + y[1]
+            res[2] = res[2][:-1]  # romaji, strip the last character
+            if len(res[2]) == 2:  # if shi, chi or dji
+                res[2] += y[2][-1]  # attach only the vowel (a/u/o) => sha/shu/sho
             else:
-                res[2] += y[2]  # attach the entire ya/yu/yo => gya/gyu/gyo
+                if res[2] == "j":  # if ji
+                    res[2] += y[2][-1]  # then attach only the vowel (a/u/o) => ja/ju/jo
+                else:
+                    res[2] += y[2]  # attach the entire ya/yu/yo => gya/gyu/gyo
+    # u and fu
+    if (idx == 2 or idx == 22) and random.random() < 0.15:
+        if idx == 2:
+            # u => we, wi
+            v = random.choice([["ぃ", "ィ", "i"], ["ぇ", "ェ", "e"]])
+            res[0] += v[0]
+            res[1] += v[1]
+            res[2] = "we" if v[2] == "e" else "wi"
+        else:
+            # fu => fa, fi, fe, fo
+            v = random.choice(
+                [["ぁ", "ァ", "a"], ["ぃ", "ィ", "i"], ["ぇ", "ェ", "e"], ["ぉ", "ォ", "o"]]
+            )
+            res[0] += v[0]
+            res[1] += v[1]
+            res[2] = res[2][0] + v[2]  # fu => fa
     # characters with small tsu attachable
     if (4 < idx < 25 or 42 < idx) and random.random() < 0.15:
         res[0] = "っ" + res[0]
@@ -115,16 +144,5 @@ def random_kana():
 
 
 if __name__ == "__main__":
-    # x, y, z = [], [], []
-    # for j in range(random.randint(3, 8)):
-    #     r = random_kana()
-    #     x += r[0]
-    #     y += r[1]
-    #     z += r[2]
-    # print("".join(x))
-    # print("".join(y))
-    # print("".join(z))
     while True:
-        k = random_kana()
-        print(k[0])
-        print(k[2])
+        print(random_kana())
